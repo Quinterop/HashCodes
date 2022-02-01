3a65725ddd05879d79a1fc2ab069345a800671ff
LE FRANC matthieu <matt.lefranc@gmail.com>
2022-02-01 17:44:06 +0100

    récupération du contenu du fichier passé en paramètre

---
+++ TPs/TP2/Parsing.java
@@ -0,0 +1,44 @@
+package TPs.TP2;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Parsing {
+    
+    public static void main(String[] args) {
+        
+        File f = new File(args[1]);
+        
+        /*
+         *
+         */
+        int nbVilles, busA, busB, busC = 0;
+        int ficNumLine = 0;
+        ArrayList<String> ficLines = new ArrayList<String>();
+        HashMap<String, Departs> villesDep = new HashMap<String, Departs>();
+
+        try {
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine()) {
+                ficLines.add(sc.nextLine());
+            }
+        }
+
+        for (int i = 0 ; i<ficLines.size() ; i++) {
+            if (i == 0) {
+                nbVilles = Integer.valueOf(ficLines.get(i));
+            }
+            if (i == 1) {
+                busA = Integer.valueOf(ficLines.get(i));
+            }
+            if (i == 2) {
+                busB = Integer.valueOf(ficLines.get(i));
+            }
+            if (i == 3) {
+                busC = Integer.valueOf(ficLines.get(i));
+            }
+        }
+    }
+}
