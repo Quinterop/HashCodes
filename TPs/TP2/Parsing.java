e93572ebbbae1a3b0e32b7b8ef2f01bf1ef3d8ac
LE FRANC matthieu <matt.lefranc@gmail.com>
2022-02-01 18:36:11 +0100

    première partie du parsing (villes de départs et crédits) plus commentaire

--- TPs/TP2/Parsing.java
+++ TPs/TP2/Parsing.java
@@ -10,20 +10,37 @@ public class Parsing {
     
     public static void main(String[] args) {
         
+        // vérification qu'un argument (chemin) a bien été passé
         if (args.length < 1) {
             System.out.println("Préciser le chemin du fichier...");
             System.exit(1);
         }
-        File f = new File(args[1]);
         
         /*
-         *
+         * déclaration des variables
          */
-        int nbVilles, busA, busB, busC = 0;
-        int ficNumLine = 0;
+
+        // ouverture du fichier passé en argument
+        File f = new File(args[1]);
+        
+        // nombre de bus pour chaque type
+        int busA, busB, busC = 0;
+
+        // nombre de villes sur la ligne
+        int nbVilles = 0;
+
+        // Liste de toutes les lignes du 
         ArrayList<String> ficLines = new ArrayList<String>();
+
+        // tableau pour stocker chaque mot de la ligne courante
+        String[] lineSplited;
+
+        // Liste des villes de départs (clé String) associées à un objet (Depart) comportant le crédit pour les bus de types A, B, C
         HashMap<String, Depart> villesDep = new HashMap<String, Depart>();
 
+
+
+        // lecture du fichier et ajout de chaque ligne dans l'ArrayList de type String
         try {
             Scanner sc = new Scanner(f);
             while(sc.hasNextLine()) {
@@ -33,23 +50,42 @@ public class Parsing {
             e.printStackTrace();
         }
 
+        // parcours de la liste de lignes
         for (int i = 0 ; i<ficLines.size() ; i++) {
+
+            // conversion de la ligne courante en un tableau contenant chaque mot de la ligne
+            lineSplited = split_on_char(ficLines.get(i));
+
+            // quatres premières lignes (de type int)
             if (i == 0) {
-                nbVilles = Integer.valueOf(ficLines.get(i));
+                nbVilles = Integer.valueOf(lineSplited[0]);
+                continue;
             }
             if (i == 1) {
-                busA = Integer.valueOf(ficLines.get(i));
+                busA = Integer.valueOf(lineSplited[0]);
+                continue;
             }
             if (i == 2) {
-                busB = Integer.valueOf(ficLines.get(i));
+                busB = Integer.valueOf(lineSplited[0]);
+                continue;
             }
             if (i == 3) {
-                busC = Integer.valueOf(ficLines.get(i));
+                busC = Integer.valueOf(lineSplited[0]);
+                continue;
             }
-            System.out.println(ficLines);
+
+            // Pour la matrice d'adjacense
+            if (nbVilles == 0) {
+                continue;
+            }
+
+            // ajout des villes de départs de leurs caractéristiques dans la HashMap
+            villesDep.put(lineSplited[0], new Depart(Integer.valueOf(lineSplited[1]), Integer.valueOf(lineSplited[2]), Integer.valueOf(lineSplited[3])));
+            nbVilles--;
         }
     }
 
+    // Pour convertir la chaine de caractère en tableau
     public static String[] split_on_char(String line) {
         return line.split(" ");
     }
