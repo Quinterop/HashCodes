7be9dc78a808f5a430d69fab30062d74fae50bdd
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-02-03 11:46:52 +0100

    nouveau model static

--- TPs/TP2/Main.java
+++ TPs/TP2/Main.java
@@ -6,51 +6,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Parsing {
+public class Main {
+
+    /*
+     * déclaration des variables
+     */
+
+    // nombre de bus pour chaque type
+    private static int busA, busB, busC = 0;
+    // nombre de villes sur la ligne
+    private static int nbVilles = 0;
+
+    // Liste de toutes les lignes du 
+    private static ArrayList<String> ficLines = new ArrayList<String>();
+
+    // tableau pour stocker chaque mot de la ligne courante
+    private static String[] lineSplited;
     
-    public static void main(String[] args) {
-        
-        // vérification qu'un argument (chemin) a bien été passé
-        if (args.length < 1) {
-            System.out.println("Préciser le chemin du fichier...");
-            System.exit(1);
-        }
-        
-        /*
-         * déclaration des variables
-         */
+    private static int id = 0;
 
+    // Liste des villes de départs (clé String) associées à un objet (Depart) comportant le crédit pour les bus de types A, B, C
+    private static HashMap<String, Depart> villesDep = new HashMap<String, Depart>();
+    private static HashMap<String,Integer> identifiants = new HashMap<String,Integer>();
+    
+    //moche mais devrait marcher
+    private static int[][] adja = new int[0][0];
+
+
+    // Pour convertir la chaine de caractère en tableau
+    public static String[] split_on_char(String line) {
+        return line.split(" ");
+    }
+
+    private static void parsing(String path) {
         // ouverture du fichier passé en argument
-        File f = new File(args[0]);
-        
-        // nombre de bus pour chaque type
-        int busA, busB, busC = 0;
-
-        // nombre de villes sur la ligne
-        int nbVilles = 0;
-
-        // Liste de toutes les lignes du 
-        ArrayList<String> ficLines = new ArrayList<String>();
-
-        // tableau pour stocker chaque mot de la ligne courante
-        String[] lineSplited;
-        
-        int id = 0;
-
-        // Liste des villes de départs (clé String) associées à un objet (Depart) comportant le crédit pour les bus de types A, B, C
-        HashMap<String, Depart> villesDep = new HashMap<String, Depart>();
-        HashMap<String,Integer> identifiants = new HashMap<String,Integer>();
-        
-        //moche mais devrait marcher
-        int[][] adja = new int[0][0];
-
-
+        File f = new File(path);
 
         // lecture du fichier et ajout de chaque ligne dans l'ArrayList de type String
         try {
-            Scanner sc = new Scanner(f);
-            while(sc.hasNextLine()) {
-                ficLines.add(sc.nextLine());
+            try (Scanner sc = new Scanner(f)) {
+                while(sc.hasNextLine()) {
+                    ficLines.add(sc.nextLine());
+                }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -81,10 +78,8 @@ public class Parsing {
                 // Pour la matrice d'adjacence
             	int x = identifiants.get(lineSplited[0]);
                 int y = identifiants.get(lineSplited[1]);
-                adja[x][y] = Integer.valueOf(lineSplited[2]);
-            	
-            		
-            		}
+                adja[x][y] = Integer.valueOf(lineSplited[2]);           	
+            }
 
             else {
                 // ajout des villes de départs de leurs caractéristiques dans la HashMap et la map des id
@@ -93,13 +88,17 @@ public class Parsing {
                 id++;
                 if (id>nbVilles) System.out.println("c pété");
             }
-            // DEBBUG : System.out.println(ficLines.get(i));
+            /* DEBBUG : System.out.println(ficLines.get(i));*/
         }
     }
 
-    // Pour convertir la chaine de caractère en tableau
-    public static String[] split_on_char(String line) {
-        return line.split(" ");
+    public static void main(String[] args) {
+        // vérification qu'un argument (chemin) a bien été passé
+        if (args.length < 1) {
+            System.out.println("Préciser le chemin du fichier...");
+            System.exit(1);
+        }
+        parsing(args[0]);
     }
 
 }
