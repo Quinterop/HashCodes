e8626fba8b178db1151366f5b2ac043d1fa7bf2a
Alass <alassane.paris@gmail.com>
2022-02-08 17:41:52 +0100

    algo

---
+++ TPs/TP3/data/brute.java
@@ -0,0 +1,45 @@
+public class brute {
+    public void brute() {
+        int winIndex = Integer.MAX_VALUE;
+        int winLength = Integer.MAX_VALUE;
+        int winString;
+        
+        
+        int searchSize = biggestWord();
+        for(int j = 0; j<mots.length ; j++){ //pour tous mots
+            
+            
+            int[] occurences = allOccurences(alphabet,mots[j]); //pour toutes occurences
+            for(int i = 0; i < occurences.length ; i++){
+                
+                int debut = occurences[i];
+                String searchString = alphabet.substring(debut, debut+searchSize); //chercher si le string de taille x contient les mots
+                
+                boolean found = true;
+                for(int k = 0; k<mots.length ; k++){
+                    if(!searchString.contains(mots[k])){ 
+                        found = false;
+                    }
+                }
+                if(found==true){
+                    winIndex = debut;
+                    winLength = searchSize;
+                    winString = searchString; //si oui ok
+                    return;
+                }else{
+                    searchSize++; //si non on augmente 
+                }
+            }
+        }
+    }
+    
+    public int biggestWord(){
+        int len = 0;
+        for(int i = 0; i<mots.length; i++){
+            if(String.length(mots[i])>len)
+            len = String.length(mots[i]);
+        }
+        return len;
+    }
+    
+}
