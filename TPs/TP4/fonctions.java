cf2c30e243d5d723e3b593cbf9245009a9e14ee4
Alass <alassane.paris@gmail.com>
2022-02-22 17:37:09 +0100

    a finir avec commandes

--- TPs/TP4/fonctions.java
+++ TPs/TP4/fonctions.java
@@ -1,5 +1,16 @@
 public class fonctions {
+    
     public int poidsTrajet(int xA, int yA, int xB, int yB) {
         return sqrt(abs(xA - xB) ^ 2 + abs(yA - yB) ^ 2);
     }
+}
+
+public string loadDrone(Drone d, Warehouse w){
+    if(d.x!=w.x || d.y!=w.y){
+        d.x = w.x;
+        d.y = w.y;
+    }
+        if(//capacité > load) {
+            //ajouter load a capacité et le retirer a w
+        }
 }
