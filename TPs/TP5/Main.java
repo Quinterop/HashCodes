28f0b02058f9b0e9d788130f01f1b41702edf309
JACOTOT Hugo <hjacotot@free.fr>
2022-03-15 16:52:21 +0100

    skeleton

---
+++ TPs/TP5/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	int arcs = 0;
+	int aretes = 0;
+	int joueurs = 0;
+	int equipes = 0;
+	int[][] matrix;
+
+	public static void main(String args[]) {
+			
+	}
+
+	public static void pcc() {
+		
+	}
+
+	public void parsing() {
+		
+	}
+}
