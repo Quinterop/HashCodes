06d9e8b46a6dfbb05f0a4d566e060f1779bc3b6a
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-03-18 15:59:38 +0100

    Dijkdtra another way

--- TPs/TP5/Main.java
+++ TPs/TP5/Main.java
@@ -24,12 +24,14 @@ public class Main {
 	public static void main(String args[]) {
 		parsing(args[0]);
 		//printMatrix();
-		int[] res=dijkstra(matrix, nbrPlayers+1);
+		/*int[] res=dijkstra(matrix, nbrPlayers+1);
 		for(int i=0;i<res.length;i++){
 			System.out.print(res[i]+" ");
 		}
 		System.out.println();
-        writeOutput(res);
+        writeOutput(res); */
+		
+        dijkstra(matrix, nbrPlayers);
 	}
 
 	private static void parsing(String path) {
@@ -59,7 +61,7 @@ public class Main {
 				nbrParty = ficLines.get(i)[2];
 				nbrEdges = ficLines.get(i)[3];
 				matrix = new int[nbrVertex][nbrVertex];
-				initMatrix(matrix);
+				//initMatrix(matrix);
 				continue;
 			}
 			matrix[ficLines.get(i)[0]-1][ficLines.get(i)[1]-1] = ficLines.get(i)[2];
@@ -72,7 +74,7 @@ public class Main {
 		for(int i=0;i<joueursEquipes.length;i++) {
 			int poidsJoueur = 0;
 			for(int j=i+1;j<joueursEquipes.length;j++) {
-				poidsJoueur += distanceFromArbitre[joueursEquipes[j]] + distanceToArbitre[joueursEquipes[j]];
+				poidsJoueur += distanceFromArbitre[joueursEquipes[j]] + distanceToArbitre[joueursEquipes[i]];
 			}
 			total += poidsJoueur;
 		}
@@ -99,6 +101,16 @@ public class Main {
 		}
 	}
 
+	public static int[][] reverseGraph(int [][] src) {
+			int[][] res = new int[src.length][src.length];
+			for(int i=0;i<res.length;i++) {
+				for(int j=0;j<res.length;j++) {
+					res[i][j] = src[j][i];
+				}
+			}
+			return res;
+	}
+
         // écriture du résultat dans un fichier res.out
         private static void writeOutput(int res[]) {
             try {
@@ -123,8 +135,11 @@ public class Main {
             }
         }
 
+
+	/// ----------------- DEBUT VERSION1 ----------------
+
 	//Renvoie le tableau contenant le plus petit chemin reliant chaque sommet à l'arbitre
-	public static int[] dijkstra(int[][] matrice, int arbitre){
+	public static int[] dijkstra2(int[][] matrice, int arbitre){
 		//boolean isfinished=false;
 		int [] P=new int[matrice.length-1];
 		for(int i=0;i<matrice[1].length-1;i++){
@@ -138,17 +153,6 @@ public class Main {
 		// }
 		return P;
 	}
-
-	//Détermine si le tableau de réponse à bien été rempli (cette fonction n'est pas nécéssaire)
-	/*public static boolean isNotFinished(int[] t){
-	  for(int i=0;i<t.length;i++){
-	  if(t[i]==-1){
-	  return false;
-	  }
-	  }
-	  return true;
-	  }*/
-
 
 	//Calcule la distance de tous les points par rapport à départ
 	public static int distance(int[][] matrice,int position,int depart){
@@ -189,5 +193,77 @@ public class Main {
 			}
 		}
 	}
+
+
+	/// ----------------- FIN VERSION2 ----------------
+    
+	/// ----------------- DEBUT VERSION1 ----------------
+	private static int minDistance(int dist[], Boolean sptSet[])
+    {
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index = -1;
+ 
+        for (int v = 0; v < matrix.length; v++)
+            if (sptSet[v] == false && dist[v] <= min) {
+                min = dist[v];
+                min_index = v;
+            }
+ 
+        return min_index;
+    }
+ 
+    // A utility function to print the constructed distance array
+    private static void printSolution(int dist[])
+    {
+        System.out.println("Vertex \t\t Distance from Source");
+        for (int i = 0; i < matrix.length; i++)
+            System.out.println(i + " \t\t " + dist[i]);
+    }
+ 
+    // Function that implements Dijkstra's single source shortest path
+    // algorithm for a graph represented using adjacency matrix
+    // representation
+    private static void dijkstra(int graph[][], int src)
+    {
+        int dist[] = new int[matrix.length]; // The output array. dist[i] will hold
+        // the shortest distance from src to i
+ 
+        // sptSet[i] will true if vertex i is included in shortest
+        // path tree or shortest distance from src to i is finalized
+        Boolean sptSet[] = new Boolean[matrix.length];
+ 
+        // Initialize all distances as INFINITE and stpSet[] as false
+        for (int i = 0; i < matrix.length; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
+ 
+        // Distance of source vertex from itself is always 0
+        dist[src] = 0;
+ 
+        // Find shortest path for all vertices
+        for (int count = 0; count < matrix.length - 1; count++) {
+            // Pick the minimum distance vertex from the set of vertices
+            // not yet processed. u is always equal to src in first
+            // iteration.
+            int u = minDistance(dist, sptSet);
+ 
+            // Mark the picked vertex as processed
+            sptSet[u] = true;
+ 
+            // Update dist value of the adjacent vertices of the
+            // picked vertex.
+            for (int v = 0; v < matrix.length ; v++)
+ 
+                // Update dist[v] only if is not in sptSet, there is an
+                // edge from u to v, and total weight of path from src to
+                // v through u is smaller than current value of dist[v]
+                if (!sptSet[v] && graph[u][v] != 0 && dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
+                    dist[v] = dist[u] + graph[u][v];
+        }
+ 
+        // print the constructed distance array
+        printSolution(dist);
+    }
 
 }
