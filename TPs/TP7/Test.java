66d7d1269267e59efa70f71f495ef47658fbde5a
lounes <lounes.is3@gmail.com>
2022-04-05 16:55:36 +0200

    commit

--- TPs/TP7/Test.java
+++ TPs/TP7/Test.java
@@ -48,6 +48,14 @@ public class Test{
         //return bufferNotes.size()*(currentNote / somme(notes)) == getNbrOcc(currentNote);
 
 }
+    public static boolean isOk(){
+        for(int i=0;i<notes.length;i++){
+            if(!isJouable(i)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args){
         parsing(args[0]);
@@ -75,8 +83,11 @@ public class Test{
                     }
                 }
             }
-            if(bestNote==-1) { 
+            /*if(bestNote==-1) { 
                 System.out.println("BLOQUE");
+                break;
+            }*/if(!isOk()){
+                System.out.println("BLOBLO");
                 break;
             }
             else{ 
@@ -85,6 +96,7 @@ public class Test{
                 nbrJouees++;
                 lastNote = bestNote;
             }
+            
         }
         print("Liste occurence par note (si) : ", si);
         System.out.println("Dernière note jouée (ai) : " + notes[lastNote]);
