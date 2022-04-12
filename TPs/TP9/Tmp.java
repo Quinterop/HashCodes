8d4fea4bcb50c7baac562ce7fd8b9022442954c5
Matthieu Le Franc <matt.lefranc@gmail.com>
2022-04-12 17:02:14 +0200

    début développement solution basique

---
+++ TPs/TP9/Tmp.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class Tmp {
+    int[] dTab = new int[5];
+    HashMap<Integer, int[]> dominos = new HashMap<Integer, int[]>();
+    LinkedList<int[]> results = new LinkedList<>();
+
+    private void testConfig() {
+        for(HashMap<Integer, int[]> d : dominos.keySet()) {
+            
+        }
+    }
+}
